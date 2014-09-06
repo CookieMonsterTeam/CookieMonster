c54ecefe4b4f7cd514272bef342b8e0d717df951
Aktanusa <chanam.geo@yahoo.com>
2014-09-06 16:18:06 -0400

    Hopefully fix jscolor not loading before CM.Init is called (Thanks The_omniscent_pie)

--- src/Main.js
+++ src/Main.js
@@ -45,7 +45,7 @@ CM.ReplaceNative = function() {
 	
 	CM.Backup.UpdateMenu = Game.UpdateMenu;
 	Game.UpdateMenu = function() {
-		if (jscolor == undefined || jscolor.picker == undefined || jscolor.picker.owner == undefined) {
+		if (jscolor.picker == undefined || jscolor.picker.owner == undefined) {
 			CM.Backup.UpdateMenu();
 			CM.Disp.AddMenu();
 		}
@@ -107,26 +107,36 @@ CM.Init = function() {
 	}
 	if (proceed) {
 		CM.Disp.AddJscolor();
-		CM.Disp.CreateCssArea();
-		CM.Disp.CreateBotBar();
-		CM.Disp.CreateTimerBar();
-		CM.Disp.CreateUpgradeBar();
-		CM.Disp.CreateWhiteScreen();
-		CM.Disp.CreateGCTimer();
-		CM.Disp.CreateResetTooltip();
-		CM.Disp.CreateTooltipWarnCaut();
-		CM.Disp.AddTooltipBuild();
-		CM.Disp.AddTooltipBuild10();
-		CM.Disp.AddWrinklerAreaDetect();
-		CM.ReplaceNative();
-		Game.CalculateGains();
-		CM.LoadConfig(); // Must be after all things are created!
-
-		if (Game.prefs.popups) Game.Popup('Cookie Monster version ' + CM.VersionMajor + '.' + CM.VersionMinor + ' loaded!');
-		else Game.Notify('Cookie Monster version ' + CM.VersionMajor + '.' + CM.VersionMinor + ' loaded!','','',1);
 		
-		Game.Win('Third-party');
+		var delay = setInterval(function() {
+			if (jscolor != undefined) {
+			CM.DelayInit();
+			clearInterval(delay);
+			}
+		}, 500);
 	}
+}
+
+CM.DelayInit = function() {
+	CM.Disp.CreateCssArea();
+	CM.Disp.CreateBotBar();
+	CM.Disp.CreateTimerBar();
+	CM.Disp.CreateUpgradeBar();
+	CM.Disp.CreateWhiteScreen();
+	CM.Disp.CreateGCTimer();
+	CM.Disp.CreateResetTooltip();
+	CM.Disp.CreateTooltipWarnCaut();
+	CM.Disp.AddTooltipBuild();
+	CM.Disp.AddTooltipBuild10();
+	CM.Disp.AddWrinklerAreaDetect();
+	CM.ReplaceNative();
+	Game.CalculateGains();
+	CM.LoadConfig(); // Must be after all things are created!
+
+	if (Game.prefs.popups) Game.Popup('Cookie Monster version ' + CM.VersionMajor + '.' + CM.VersionMinor + ' loaded!');
+	else Game.Notify('Cookie Monster version ' + CM.VersionMajor + '.' + CM.VersionMinor + ' loaded!','','',1);
+
+	Game.Win('Third-party');
 }
 
 CM.ConfigDefault = {BotBar: 1, TimerBar: 1, TimerBarPos: 0, BuildColor: 1, UpBarColor: 1, Flash: 1, Sound: 1,  Volume: 100, GCSoundURL: 'http://freesound.org/data/previews/66/66717_931655-lq.mp3', SeaSoundURL: 'http://www.freesound.org/data/previews/121/121099_2193266-lq.mp3', GCTimer: 1, Title: 1, Tooltip: 1, ToolWarnCaut: 1, ToolWarnCautPos: 1, ToolWrink: 1, Stats: 1, UpStats: 1, SayTime: 1, Scale: 2, StatsPref: {Lucky: 1, Chain: 1, HC: 1, Wrink: 1, Sea: 1}, Colors : {Blue: '#4bb8f0', Green: '#00ff00', Yellow: '#ffff00', Orange: '#ff7f00', Red: '#ff0000', Purple: '#ff00ff', Gray: '#b3b3b3'}};
