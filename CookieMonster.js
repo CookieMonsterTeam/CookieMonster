4d2287a7aea4b065a9d39d2dcbdb7381cf4fe445
Aktanusa <chanam.geo@yahoo.com>
2016-04-28 17:56:46 -0400

    Change to Version 2.6

--- CookieMonster.js
+++ CookieMonster.js
@@ -2482,7 +2482,7 @@ CM.ConfigDefault = {BotBar: 1, TimerBar: 1, TimerBarPos: 0, BuildColor: 1, BulkB
 CM.ConfigPrefix = 'CMConfig';
 
 CM.VersionMajor = '2';
-CM.VersionMinor = '5';
+CM.VersionMinor = '6';
 
 /*******
  * Sim *
--- src/Main.js
+++ src/Main.js
@@ -209,5 +209,5 @@ CM.ConfigDefault = {BotBar: 1, TimerBar: 1, TimerBarPos: 0, BuildColor: 1, BulkB
 CM.ConfigPrefix = 'CMConfig';
 
 CM.VersionMajor = '2';
-CM.VersionMinor = '5';
+CM.VersionMinor = '6';
 
