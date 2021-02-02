769f7f18dd2e148c887d696cf9b53dd411d7dfa9
mason1920 <mason1920@protonmail.com>
2021-02-02 05:25:42 -0500

    Make a Userscript and Refer the README to It
    Tested and works with Greasemonkey, Tampermonkey, and Violentmonkey.

---
+++ cookie-monster.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name Cookie Monster
+// @include /https?://orteil.dashnet.org/cookieclicker/
+// ==/UserScript==
+
+const readyCheck = setInterval(() => {
+    const Game = unsafeWindow.Game;
+
+    if (
+        typeof Game !== "undefined" &&
+        typeof Game.ready !== "undefined" &&
+        Game.ready
+    ) {
+        Game.LoadMod(
+            "https://aktanusa.github.io/CookieMonster/CookieMonster.js"
+        );
+        clearInterval(readyCheck);
+    }
+}, 1000);
