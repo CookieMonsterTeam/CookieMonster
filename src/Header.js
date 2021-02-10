39b1ec6db4327a9a38565afc35f863ea47582866
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-02-10 20:30:50 +0100

    Resolve conflicts

--- src/Header.js
+++ src/Header.js
@@ -6,16 +6,16 @@
  * This functions creates the necessary objects and is run when they do not already exist
  */
 const CM = {
-	Backup: {},
-	Cache: {},
-	Config: {},
-	ConfigData: {},
-	Data: {},
-	Disp: {},
-	Footer: {},
-	Main: {},
-	Options: {},
-	Sim: {},
+    Backup: {},
+    Cache: {},
+    Config: {},
+    ConfigData: {},
+    Data: {},
+    Disp: {},
+    Footer: {},
+    Main: {},
+    Options: {},
+    Sim: {},
 	VersionMajor: '2.031',
 	VersionMinor: '3',
 };
