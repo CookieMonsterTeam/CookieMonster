be529d0b7176fe00b8dc685f3419770672b50de3
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-03-22 08:21:28 +0100

    Update commands and removed comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,3 @@
-/** We know this is not how Webpack should be used, but we are slowly transferring our code.
- * For now this is how at least start to create a structure that allows us to transfer towards
- * more modular code. Any help is highly appreciated.
- */
 const path = require('path');
 
 module.exports = function (env) {
