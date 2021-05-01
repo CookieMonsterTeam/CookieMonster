87e4a6821ae030c8ed109016b8ece8bdb80cdd1f
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-05-01 20:24:28 +0200

    Fix bug with expanded upgrade section

--- src/Disp/DrawHook.js
+++ src/Disp/DrawHook.js
@@ -2,15 +2,13 @@ import ToggleWrinklerButtons from '../Config/Toggles/ToggleWrinklerButtons';
 import { CMOptions } from '../Config/VariablesAndData';
 import Beautify from './BeautifyAndFormatting/Beautify';
 import UpdateBuildings from './BuildingsUpgrades/Buildings';
+import UpdateUpgradeSectionsHeight from './BuildingsUpgrades/UpdateUpgradeSectionsHeight';
 import UpdateUpgrades from './BuildingsUpgrades/Upgrades';
 import { UpdateBotBar } from './InfoBars/BottomBar';
 import { UpdateTimerBar } from './InfoBars/TimerBar';
 import RefreshMenu from './MenuSections/Refreshmenu';
 import UpdateTooltips from './Tooltips/UpdateTooltips';
-import {
-  CheckWrinklerTooltip,
-  UpdateWrinklerTooltip,
-} from './Tooltips/WrinklerTooltips';
+import { CheckWrinklerTooltip, UpdateWrinklerTooltip } from './Tooltips/WrinklerTooltips';
 
 /**
  * This function handles all custom drawing for the Game.Draw() function.
@@ -26,16 +24,14 @@ export default function CMDrawHook() {
   ) {
     const timer = document.getElementById('CMStatsAutosaveTimer');
     if (timer) {
-      timer.innerText = Game.sayTime(
-        Game.fps * 60 - (Game.T % (Game.fps * 60)),
-        4,
-      );
+      timer.innerText = Game.sayTime(Game.fps * 60 - (Game.T % (Game.fps * 60)), 4);
     }
   }
 
   // Update colors
   UpdateBuildings();
   UpdateUpgrades();
+  UpdateUpgradeSectionsHeight();
 
   // Redraw timers
   UpdateTimerBar();
@@ -58,10 +54,7 @@ export default function CMDrawHook() {
 
   // Replace Cookies counter because Orteil uses very weird code to "pad" it...
   if (CMOptions.Scale) {
-    let str = l('cookies').innerHTML.replace(
-      /.*(?=<br>)/i,
-      Beautify(Game.cookies),
-    );
+    let str = l('cookies').innerHTML.replace(/.*(?=<br>)/i, Beautify(Game.cookies));
     if (Game.prefs.monospace) str = `<span class="monospace">${str}</span>`;
     l('cookies').innerHTML = str;
   }
