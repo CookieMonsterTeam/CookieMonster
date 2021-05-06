5a045e7ac493762cba37e5eaab8276978c1f8402
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-05-06 20:27:17 +0200

    Expose building and upgrade data to global scope

--- src/Cache/PP/PP.js
+++ src/Cache/PP/PP.js
@@ -1,6 +1,7 @@
 /**
  * Section: Functions related to caching PP */
 
+import { CacheObjects1, CacheObjects10, CacheObjects100, CacheUpgrades } from '../VariablesAndData';
 import CacheBuildingsPP from './Building';
 import CacheUpgradePP from './Upgrade';
 
@@ -11,4 +12,11 @@ import CacheUpgradePP from './Upgrade';
 export default function CachePP() {
   CacheBuildingsPP();
   CacheUpgradePP();
+  window.CookieMonsterData.Objects1 = JSON.parse(JSON.stringify(CacheObjects1));
+  window.CookieMonsterData.Objects10 = JSON.parse(JSON.stringify(CacheObjects10));
+  window.CookieMonsterData.Objects100 = JSON.parse(JSON.stringify(CacheObjects100));
+  window.CookieMonsterData.Upgrades = [];
+  Object.entries(CacheUpgrades).forEach((i) => {
+    window.CookieMonsterData.Upgrades[i[0]] = JSON.parse(JSON.stringify(i[1]));
+  });
 }
