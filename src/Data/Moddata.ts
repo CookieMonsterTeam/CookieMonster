33caf8838c02d448dfcddadbdc78c8fa2459f192
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-06-27 23:22:53 +0200

    Push to 2.031.9

--- src/Data/Moddata.ts
+++ src/Data/Moddata.ts
@@ -1,7 +1,7 @@
 /** Data related directly to Cookie Monster */
 
 export const VersionMajor = '2.031';
-export const VersionMinor = '8';
+export const VersionMinor = '9';
 
 /** Information about Cookie Monster to be displayed in the info section */
 export const ModDescription = `<div class="listing">
@@ -15,17 +15,18 @@ export const ModDescription = `<div class="listing">
 
 /** Latest releasenotes of Cookie Monster to be displayed in the info section */
 export const LatestReleaseNotes = `<div class="listing">
-<b>We have moved Cookie Monster to a new location!</b></br>
-Please update the link you are using to load Cookie Monster to https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js and let everybody know this is the new place to find Cookie Monster!</br>
-These are the release notes for the latest update (v 2.031.8).</br>
-</br>
 This update implements the following functions:</br>
-- The tooltip of buildings now shows how many buildings need to be bought before a new achievement is reached and the cost and PP of this</br>
-- New setting to force the upgrades sections to always show all available upgrades, stopping them from collapsing and expanding</br>
+- For developers: we now expose some data calculated by Cookie Monster to the global scope. You can access it through the CookieMonsterData object</br>
+- The column with the most optimal building now has a green coloured indicator whenever colour coding is turned on</br>
+- The current season in the seasons statistics section is now displayed with green text for easier identification</br>
+- New option to show a timer bar that counts down till next autosave</br>
+- New option to sort buildings based on the "cost till next achievement"</br>
+- Added extra information about achievements in statistics page</br>
 </br>
 This update fixes the following bugs:</br>
-- Settings menu now correctly updates after changing a setting</br>
-- Fixed incorrect calculation of dates in rare cases</br>
-- Remove golden cookie timers when you ascend</br>
+- Fixed a bug where ignoring certain buildings in rare cases did not create a "most optimal building"</br>
+- Fixed some issues related to "left till achievement"</br>
+- Fixed some cases where upgrades and buildings were not correctly sorted</br>
+- Fixed the tooltip of "Pop all normal wrinklers" displaying an incorrect reward when Shiny's are present</br>
 </div>
 `;
