54717de002132bdc712596a174d380e7558f8244
Simon Balling <ballingsimon@googlemail.com>
2021-09-04 16:25:17 +0200

    Dragon fix

--- src/Cache/Dragon/Dragon.js
+++ src/Cache/Dragon/Dragon.js
@@ -14,7 +14,7 @@ export default function CacheDragonCost() {
       Game.dragonLevel < 25 &&
       Game.dragonLevels[Game.dragonLevel].buy.toString().includes('sacrifice')
     ) {
-      let target = Game.dragonLevels[Game.dragonLevel].buy.toString().match(/Objects\[(.*)\]/)[1];
+      let target = "";
       const amount = Game.dragonLevels[Game.dragonLevel].buy
         .toString()
         .match(/sacrifice\((.*?)\)/)[1];
