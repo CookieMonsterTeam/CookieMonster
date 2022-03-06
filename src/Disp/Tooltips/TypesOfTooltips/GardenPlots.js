79f8079228d77178aa799641634d39f67adab7c8
humdogm <humziq@gmail.com>
2022-03-06 01:33:39 -0600

    Re-order Garden max rewards by amount of cookies Build CookieMonsterDev.js and .js.map Ran npm build-dev

--- src/Disp/Tooltips/TypesOfTooltips/GardenPlots.js
+++ src/Disp/Tooltips/TypesOfTooltips/GardenPlots.js
@@ -21,14 +21,14 @@ export default function GardenPlots() {
     const reward = document.createElement('div');
     reward.id = 'CMTooltipPlantReward';
     l('CMTooltipBorder').appendChild(reward);
-    if (plantName === 'Bakeberry') {
-      l('CMTooltipPlantReward').textContent = `${
-        mature ? Beautify(Math.min(Game.cookies * 0.03, Game.cookiesPs * 60 * 30)) : '0'
-      } / ${Beautify(Game.cookiesPs * 60 * 30)}`;
-    } else if (plantName === 'Chocoroot' || plantName === 'White chocoroot') {
+    if (plantName === 'Chocoroot' || plantName === 'White chocoroot') {
       l('CMTooltipPlantReward').textContent = `${
         mature ? Beautify(Math.min(Game.cookies * 0.03, Game.cookiesPs * 60 * 3)) : '0'
       } / ${Beautify(Game.cookiesPs * 60 * 3)}`;
+    } else if (plantName === 'Bakeberry') {
+      l('CMTooltipPlantReward').textContent = `${
+        mature ? Beautify(Math.min(Game.cookies * 0.03, Game.cookiesPs * 60 * 30)) : '0'
+      } / ${Beautify(Game.cookiesPs * 60 * 30)}`;
     } else if (plantName === 'Queenbeet') {
       l('CMTooltipPlantReward').textContent = `${
         mature ? Beautify(Math.min(Game.cookies * 0.04, Game.cookiesPs * 60 * 60)) : '0'
