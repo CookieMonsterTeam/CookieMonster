29946ef98e775d4203a654a2e8061ff465bc546c
Michael <mddouros98@gmail.com>
2026-02-13 18:05:06 -0600

    Test logging

--- src/Sim/Calculations/CalculateGains.js
+++ src/Sim/Calculations/CalculateGains.js
@@ -77,6 +77,7 @@ export default function CalculateGains() {
   if (SimHas('Dragon scale')) mult *= 1.03;
 
   if (SimHas('Wrinkler ambergris')) mult *= 1.06;
+  console.log('SimHasWinklerAmbergris:', SimHas('Wrinkler ambergris'))
 
   // Check effect of chosen Gods
   let buildMult = 1;
